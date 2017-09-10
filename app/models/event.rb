23c0893be0055c3212ce5d0d6989d4608fd9e5bc
ajishnair <ajishrnair89@gmail.com>
2017-09-10 11:47:30 +0530

    Handle redis not running scenario

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,11 +8,21 @@ class Event
   belongs_to :user
 
   def is_blocked(device_id)
-  	$redis.hexists(device_id, self.name)
+    #if redis is down don't block events
+    begin
+  	  return $redis.hexists(device_id, self.name)
+    rescue Redis::BaseConnectionError => ex
+      logger.error("Redis timeout error : " + ex.message)
+      return false
+    end
   end
 
   def increment_counter(device_id)
-  	$redis.hincrby(device_id, self.name, 1)
+    begin
+  	  $redis.hincrby(device_id, self.name, 1)
+    rescue Redis::BaseConnectionError => ex
+      logger.error("Redis timeout error : " + ex.message)
+    end
   end
 
   def get_attributes
